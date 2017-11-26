82a0bde474e649cae9b1b2c57c00f585db6af2b9
cetres <cetres@gmail.com>
2017-11-26 09:25:52 -0200

    fix test script

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,7 +1,10 @@
 import sys
 import requests
 
-requests.head("http://127.0.0.1:8080")
-print("Website return code: %d" % r.status_code)
-
+try:
+    r = requests.head("http://127.0.0.1:8080")
+    print("Website return code: %d" % r.status_code)
+except requests.RequestException as e:
+    print(str(e))
+    sys.exit(1)
 sys.exit(0)
